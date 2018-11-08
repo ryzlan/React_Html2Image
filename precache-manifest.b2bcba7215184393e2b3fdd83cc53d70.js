a90d6fceac350bc898ea5183400b6fed06774b41
Ruzlan Karim <ruzlankarim2@gmail.com>
2018-11-08 17:50:02 +0600

    Updates

---
+++ precache-manifest.b2bcba7215184393e2b3fdd83cc53d70.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React_Html2Image/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "5e4760130434e728b8ef",
+    "url": "/React_Html2Image/static/css/main.f4e3ddfc.chunk.css"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React_Html2Image/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "1c40c01c4300a85890ef",
+    "url": "/React_Html2Image/static/js/1.1c40c01c.chunk.js"
+  },
+  {
+    "revision": "f0910f9666b2ddc790d7",
+    "url": "/React_Html2Image/static/js/runtime~main.f0910f96.js"
+  },
+  {
+    "revision": "5e4760130434e728b8ef",
+    "url": "/React_Html2Image/static/js/main.5e476013.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React_Html2Image/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React_Html2Image/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React_Html2Image/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1c40c01c4300a85890ef",
+    "url": "/React_Html2Image/static/css/1.bfdd4601.chunk.css"
+  },
+  {
+    "revision": "e43dc86b0405fb466fcf6203a7fe2517",
+    "url": "/React_Html2Image/index.html"
+  }
+];
